561291ba57f0f0c4c9c883e695f079be50b0dae9
JUNE SUN AUNG NJAI <nevertoolatetoreturn@gmail.com>
2019-04-29 06:06:43 +0000

    ADDED sub.txt , comment, open file and tested, it worked

--- main.c
+++ main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+
+//Global char array
 char upper_case[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-char lower_case[26]="abcdefghijklmnopqrstuvwxyz";
-char unique_key[26];
-//qwertyuiopasdfghjklzxcvbnm
+char lower_case[26]="abcdefghijklmnopqrstuvwxyz";//lower case
+char unique_key[26];//unique keys for encryption and decryption
 
-void plain_text(char* msg);
-void encryption(char* msg);
-void decryption(char* msg);
+void encryption(char* msg);// encrytion prototype
+void decryption(char* msg);// decrytion prototype
 
-int main()
+int main() //main function
 {
-int i, key, opt, options;
-char msg[1000];
+int opt, //assinging option 1
+    options; // assigning option 2
+char msg[1000]; //defining array input size or file 
 FILE *input;// assinging file pointer
 
 printf("To Manually input '1' (or) to Read from the file '2': ");//Option for input
-scanf("%d", &opt);
+scanf("%d", &opt); //getting option value
 
 while(opt != 1 && opt != 2){ // if user put other than 1 or 2 in the first attemp, the loop will start
  printf("\nInvalid input\nTo quit, enter '0' (or) To Manually input, enter '1' (or) To Read from the file, enter '2': ");
@@ -36,7 +37,7 @@ while(opt != 1 && opt != 2){ // if user put other than 1 or 2 in the first attem
      while(!feof(input)){ //making sure system reads till the end of file
      fgets(msg, 1000, input);//getting("char variable", array size, file pointer)
      }
-     fclose(input);
+     fclose(input); // this make sure file closes after using
  }
  
 printf("To Encrypt, enter '1' (or) To Decrypt, enter '2': ");//Asking option for En or De cryption
@@ -45,60 +46,61 @@ scanf("%d", &options);
 while(options != 1 && options != 2){ // The loop if user's wrong input
   printf("\nInvalid input\nTo quit, enter '0' (or) To Encrypt, enter '1' (or) To decrypt, enter '2': ");
   scanf("%d", &options); 
-    if (options == 0){ return 0;}
+    if (options == 0){ return 0;} //for input 0 to end the program
  }
+ 
 switch(options){
-    case 1:encryption(msg);
+    case 1:encryption(msg);//for input 1, it will call encrytion fucntion
            break;
-    case 2:decryption(msg);
+    case 2:decryption(msg);// for input 2, it will call decryption function
            break;
 }
-    return 0;
+    return 0; //ending the program
 }
 
+//defining encrytion function
 void encryption(char* msg){
-int i, j; 
+int i, j; //counters
 printf("Enter 26 unique key of character for encryption: ");
-scanf("%s", unique_key);
+scanf("%s", unique_key); //getting unique keys for input
 
-for(i = 0; i< strlen (msg); i++){
-    for(j = 0; j<26; j++){
-        
-        if (lower_case[j] == msg[i]){
-           printf("\nKey replaced as below\nabcdefghijklmnopqrstuvwxyz");
-           printf("\n||||||||||||||||||||||||||\n");
-           printf("%s\n",unique_key);
+//This show the chart
+printf("\nKey replaced as below\nABCDEFGHIJKLMNOPQRSTUVWXYZ\nabcdefghijklmnopqrstuvwxyz");
+printf("\n||||||||||||||||||||||||||\n");
+printf("%s\n",unique_key);
+
+
+for(i = 0; i< strlen (msg); i++){// loop will run until the end of text input
+    for(j = 0; j<26; j++){//loop will find the positions of unique key for plain text dring 26 counts
+        if (lower_case[j] == msg[i]){ 
+           msg[i] = unique_key[j]; // the value in the position of unique key will replace the value of input positon.
+           break;//every time position of input text matches normal plain text postion, the count will start again
+        }
+        else if(upper_case[j] == msg[i]){//for upper case input letter detected
            msg[i] = unique_key[j];
            break;
         }
-        else if(upper_case[j] == msg[i]){
-           printf("\nKey replaced as below\nABCDEFGHIJKLMNOPQRSTUVWXYZ");
-           printf("\n||||||||||||||||||||||||||\n");
-           printf("%s\n",unique_key);
-           msg[i] = unique_key[j];
-           break;
-        }
-        
-    }
-    
-}
-printf("Encryted text is: %s", msg);
+     }
+  }
+ printf("Encryted text is: %s", msg);// after getting the final value and print
 }
 
+
+//defining decryption function
 void decryption(char* msg){
-int i, j;
+int i, j;//counters
 printf("Enter 26 unique key of characters for decryption: ");
-scanf("%s", unique_key);
+scanf("%s", unique_key);//getting unique:
 
-  for (i = 0; i<strlen(msg);i++){
-      for(j = 0; j<26; j++){
-          if( unique_key[j] == msg[i]){
-              msg[i]=lower_case[j];
-              break;
+  for (i = 0; i<strlen(msg);i++){// loop until end of input text
+      for(j = 0; j<26; j++){// loop until positon of input matches position of unique keys
+          if( unique_key[j] == msg[i]){// if condition matches,the position of unique key will pass to plain text position
+              msg[i]=lower_case[j]; // then, value in the position of plain text will replace value in the position of output 
+              break; // once the condition met counting will start agian
             }
         }
     }
-printf("Decrypted text is: %s",msg);
+printf("Decrypted text is: %s",msg); // the value of msg is printed
 }
 
 
